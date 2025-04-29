bfd4b9bdc62a25fd9c29361b6f781e73997bc12f
Saiful Islam <saif29oct@gmail.com>
2025-04-29 14:26:52 +0600

    feat: integrate Tailwind CSS
    - made a navigation menu with Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import './index.css';
 import { FindResturant } from './Layouts/FindResturant';
+import {TestComponent} from "./components/TestComponent";
 
 function App() {
   return (
     <div className="App">
       <FindResturant />
+        {/*<TestComponent />*/}
     </div>
   );
 }
